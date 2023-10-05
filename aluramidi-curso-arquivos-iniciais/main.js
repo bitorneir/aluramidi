99b8dd9aec9dfc2a4b882a165eb65ac806acbb47
bitorneir <133902125+bitorneir@users.noreply.github.com>
2023-10-05 09:08:16 -0300

    05/10

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,30 @@
 alert('giru o mais bonito')
+
+
+document.querySelector('.tecla_pom').onclick= tocaSomPom;
+function tocaSomPom(){ document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_tim').onclick= tocaSomtim;
+function tocaSomtim(){ document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_puff').onclick= tocaSompuff;
+function tocaSompuff(){ document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_splash').onclick= tocaSomsplash;
+function tocaSomsplash(){ document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_toim').onclick= tocaSomtoim;
+function tocaSomtoim(){ document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_psh').onclick= tocaSompsh;
+function tocaSompsh(){ document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_tic').onclick= tocaSomtic;
+function tocaSomtic(){ document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tom').onclick= tocaSomtom;
+function tocaSomtom(){ document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_clap').onclick= tocaSomclap;
+function tocaSomclap(){ document.querySelector('#som_tecla_clap').play();
+}
